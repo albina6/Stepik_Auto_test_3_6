1f3a1aaf6af3b051ca558e9f0755afa9ffa76cfd
albina6 <m_ab3@mail.ru>
2024-07-15 18:14:48 +0500

    add var language

---
+++ test_items.py
@@ -0,0 +1,18 @@
+# test_items.py
+
+import time
+import selenium
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
+
+class TestProduct():
+    
+    def test_button_submit(self, browser):
+        browser.get(link)
+        time.sleep(30)
+        # submit = browser.find_element(By.CSS_SELECTOR, ".product_page button[type='submit']")
+        try:
+            browser.find_element(By.CSS_SELECTOR, ".product_page button[type='submit']")
+        except selenium.common.exceptions.NoSuchElementException:
+            raise AssertionError('Button not found')
